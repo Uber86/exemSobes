6401799d3ffc0ac4a4c80f2acce7b6197414d08c
Сергей <lockup86@rambler.ru>
2025-02-09 12:31:54 +0300

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        String string = "My name is Sergiy. I study profession developer Java IND";
+
+        String [] sort = string.split(" ");
+
+        System.out.println(Arrays.toString(sort));
+
+        String [] sortTwo = string.split(" ", 4);
+
+        System.out.println(Arrays.toString(sortTwo));
     }
 }
